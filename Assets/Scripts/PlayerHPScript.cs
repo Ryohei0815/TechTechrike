7409ac62dfee24bd2cef47c3e9ab99ec3d52e9e9
LD9-RYOHEI-KITO\Ryohei <tagatame1996@yahoo.co.jp>
2017-09-22 10:01:37 +0900

    最初のコミット

---
+++ Assets/Scripts/PlayerHPScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHPScript : MonoBehaviour {
+
+	public int playerHP = 100;
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("敵のHPは" + playerHP);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (playerHP == 0) {
+			Debug.Log ("GameOver");
+		}
+		
+	}
+
+	private void OnTriggerEnter(Collider other){
+		playerHP--;
+		Debug.Log ("プレイヤーのHPは" + playerHP);
+	}
+
+}
